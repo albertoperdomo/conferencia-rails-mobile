9b079bd792232116bcde9a82c6df54365c9b4fca
Alberto Perdomo <alberto.perdomo@aentos.com>
2011-07-13 17:18:50 +0200

    Initial commit with Rails Admin.

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,1 @@
+RailsAdmin.authenticate_with {}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConferenciarailsMobile::Application.config.secret_token = '8d84813ad8201cdc642c1a9821c9b53cacceda64688724a33f29e90d4ffaaac535213fd755d80749cd09f048eef9f6845026d4361c1e1aaf936a19b276f9bbaf'
